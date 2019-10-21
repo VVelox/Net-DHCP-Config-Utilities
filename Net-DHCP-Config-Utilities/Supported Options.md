8d69d1cba3edd9e4d0ad159f48403a606274fcd0
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-10-21 04:55:34 -0500

    more documentation and add a MD file for helping with keping the options up to date

---
+++ Net-DHCP-Config-Utilities/Supported Options.md
@@ -0,0 +1,17 @@
+# Options
+
+| Code | Name             | Multi | Type | Long Name           |
+|------|------------------|-------|------|---------------------|
+| 0    | mask             | 0     | IP   | subnet-mask         |
+| 1    | time-offset      | 0     | INT  | time-offset         |
+| 3    | routers          | 1     | IP   | routers             |
+| 4    | ntp              | 1     | IP   | time-servers        |
+| 6    | dns              | 1     | IP   | domain-name-servers |
+| 17   | root             | 0     | TXT  | root-path           |
+| 26   | mtu              | 0     | INT  | interface-mtu       |
+| 28   | broadcast        | 0     | IP   | broadcast-address   |
+| 51   | lease-time       | 0     | INT  | dhcp-lease-time     |
+| 66   | tfp-server       | 0     | TXT  | tftp-server-name    |
+| 67   | bootfile         | 0     | TXT  | bootfile-name       |
+| 213  | v4-access-domain | 0     | TXT  | v4-access-domain    |
+| 252  | web-proxy        | 0     | TXT  | web-proxy           |
--- Net-DHCP-Config-Utilities/lib/Net/DHCP/Config/Utilities/Options.pm
+++ Net-DHCP-Config-Utilities/lib/Net/DHCP/Config/Utilities/Options.pm
@@ -7,7 +7,7 @@ use Net::CIDR;
 
 =head1 NAME
 
-Net::DHCP::Config::Utilities::Options - 
+Net::DHCP::Config::Utilities::Options - Helper utilities for working with DHCP options.
 
 =head1 VERSION
 
@@ -23,6 +23,15 @@ our $VERSION = '0.0.1';
     use Net::DHCP::Config::Utilities::Options;
     
     my $dhcp_options=Net::DHCP::Config::Utilities::Options->new;
+    
+    my $options=$dhcp_options->get_options;
+    use Data::Dumper;
+    print Dumper( $options );
+
+    my $error=$dhcp_options->validate_option( 'dns', '192.168.0.1 , 10.10.10.10' );
+    if ( defined( $error ) ){
+        die( $error );
+    }
 
 =head1 METHODS
 
@@ -422,6 +431,26 @@ sub validate_option{
 
 	return undef;
 }
+
+=head1 SUPPORT OPTIONS
+
+This only supports the more commonly used one for now and avoids the out of date ones.
+
+    | Code | Name             | Multi | Type | Long Name           |
+    |------|------------------|-------|------|---------------------|
+    | 0    | mask             | 0     | IP   | subnet-mask         |
+    | 1    | time-offset      | 0     | INT  | time-offset         |
+    | 3    | routers          | 1     | IP   | routers             |
+    | 4    | ntp              | 1     | IP   | time-servers        |
+    | 6    | dns              | 1     | IP   | domain-name-servers |
+    | 17   | root             | 0     | TXT  | root-path           |
+    | 26   | mtu              | 0     | INT  | interface-mtu       |
+    | 28   | broadcast        | 0     | IP   | broadcast-address   |
+    | 51   | lease-time       | 0     | INT  | dhcp-lease-time     |
+    | 66   | tfp-server       | 0     | TXT  | tftp-server-name    |
+    | 67   | bootfile         | 0     | TXT  | bootfile-name       |
+    | 213  | v4-access-domain | 0     | TXT  | v4-access-domain    |
+    | 252  | web-proxy        | 0     | TXT  | web-proxy           |
 
 =head1 AUTHOR
 
